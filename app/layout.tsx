9b680f3b42c02fea9163409afbdd1a065dbab94e
Azis Toriq Maulana <49329135+AzisToriq@users.noreply.github.com>
2025-07-18 06:16:34 +0700

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,25 @@
-import Image from "next/image";
 import type { Metadata } from "next";
 import { Poppins as FontSans } from "next/font/google";
 import "@/app/styles/globals.css";
 
 const fontSans = FontSans({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  variable: "--font-sans",
+	subsets: ["latin"],
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+	variable: "--font-sans",
 });
-
 export const metadata: Metadata = {
-  title: "Toriq Ganteng | Portfolio",
-  description: "Portofolio Azis Toriq Maulana",
+	title: "Toriq | Portfolio",
+	description: "Portofolio | Azis Toriq Maulana",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={fontSans.variable}>
-        {/* Contoh header logo */}
-        <header style={{ padding: "10px", display: "flex", alignItems: "center" }}>
-          <Image
-            src="/toriq.jpg"
-            alt="Logo Toriq"
-            width={50}
-            height={50}
-            priority
-          />
-          <h1 style={{ marginLeft: "10px" }}>Toriq Ganteng</h1>
-        </header>
-
-        {/* Konten utama */}
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={fontSans.variable}>{children}</body>
+		</html>
+	);
 }
